2d2b9d80eb348fffb2a9a464d85603442a9cd6a7
Jerry-Mubiru <40108767+Jerry-Mubiru@users.noreply.github.com>
2023-11-01 11:51:47 -0400

    Abstract Class definitions

---
+++ Challenge.cs
@@ -0,0 +1,8 @@
+public abstract class Challenge
+{
+    //This abstract class represents a Generic Challenge that is presented to the player
+
+    //stores the Question associated with this challenge
+    public Question challenge_question;
+    
+}
