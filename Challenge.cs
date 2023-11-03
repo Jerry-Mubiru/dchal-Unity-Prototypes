23d5ad891e09e310e9c2cebf1740de12572f73e2
Jerry-Mubiru <40108767+Jerry-Mubiru@users.noreply.github.com>
2023-11-03 16:26:20 -0400

    Abstract method implementations

--- Challenge.cs
+++ Challenge.cs
@@ -4,5 +4,13 @@ public abstract class Challenge
 
     //stores the Question associated with this challenge
     public Question challenge_question;
+
+    //abstract methods that are implemented by all challenges
+    //called when a challenge is started
+    public abstract void StartChallenge();
+    //called when a challenge is completed
+    public abstract void FinishChallenge();
+    //called when the player gets a wrong answer on a challenge
+    public abstract void FailChallenge();
     
 }
